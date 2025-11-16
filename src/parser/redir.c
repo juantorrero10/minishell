945817853b91588d36e0419428258e404f25ac2c
Juan Torrero <juantorrero10@gmail.com>
2025-11-16 19:17:50 +0100

    nueva rama

---
+++ src/parser/redir.c
@@ -0,0 +1,6 @@
+#ifndef PARSER_REDIR_C_
+#define PARSER_REDIR_C_
+
+// Redirection handling functions for the parser module
+
+#endif // PARSER_REDIR_C_
---
+++ src/parser/testdemo.c
@@ -0,0 +1,8 @@
+#include <mshparser.h>
+
+
+int main(void)
+{
+    printf("parser test demo\n");
+    return (0);
+}
